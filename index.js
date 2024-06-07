7ac6357163e285c37bd27217958e732c07354956
laharikagupta <racharlalaharika@gmai.com>
2024-06-07 10:36:56 +0000

    second react code

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  <div className="background">
+    <h1>Congratulations</h1>
+    <div className="sub-background">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="img"
+      />
+
+      <h2>Kiran V</h2>
+      <p>Vishnue Institute Of Computer Education and Technology,Bhimavaram</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
