9dcc6715f7bd91581471a29678c895985cab1c1c
laharikagupta <racharlalaharika@gmai.com>
2024-06-07 10:23:41 +0000

    all test cases passed for this code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const element = (
-//   Write your code here.
-)
+  <div className="background">
+    <h1>Super Over League</h1>
+    <div className="sub-back">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById("root"));
